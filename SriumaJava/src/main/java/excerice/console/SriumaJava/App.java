7cc63264160b45ed2994266bcf72015f2ceaa234
umasumathi57 <umasumathi57@gmail.com>
2021-12-08 10:44:33 +0530

    Project creation

---
+++ SriumaJava/src/main/java/excerice/console/SriumaJava/App.java
@@ -0,0 +1,13 @@
+package excerice.console.SriumaJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
