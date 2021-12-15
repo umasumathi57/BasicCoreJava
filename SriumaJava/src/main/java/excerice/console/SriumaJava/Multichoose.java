301cee00ddde61aeaf79e4e19289143291820b4e
umasumathi57 <umasumathi57@gmail.com>
2021-12-15 15:02:14 +0530

    Multichoose type

---
+++ SriumaJava/src/main/java/excerice/console/SriumaJava/Multichoose.java
@@ -0,0 +1,26 @@
+package excerice.console.SriumaJava;
+
+public class Multichoose
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello");
+	}
+}
+//sub class 
+class Krk
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome");
+	}
+}
+
+// sub class
+class Om
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Thank you");
+	}
+}
