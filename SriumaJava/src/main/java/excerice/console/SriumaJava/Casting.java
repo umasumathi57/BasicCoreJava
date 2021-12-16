5af2b3155ad867a40bdbe1de1d176231ced57186
umasumathi57 <umasumathi57@gmail.com>
2021-12-16 10:13:26 +0530

    Casting prgm

---
+++ SriumaJava/src/main/java/excerice/console/SriumaJava/Casting.java
@@ -0,0 +1,32 @@
+package excerice.console.SriumaJava;
+
+// Casting << Converting data- It is used between only Data types, but we can store data in Wrapper class >>
+
+public class Casting {
+	
+		public static void main (String[] Sumi)
+		{
+	   // Integer to float conversion 
+			
+			/*int a=10;
+		
+	    System.out.println((float)a);
+	    
+	    System.out.println((long)a);
+		*/
+			
+			Integer a=100;
+			System.out.println((float)a);
+			System.out.println((double)a);	
+		// ASCII conversion
+			
+			/*
+			char a='A';
+			System.out.println((int)a);
+			
+			char temp=65;
+			System.out.println((char)temp);
+			*/
+	    }
+		
+}
