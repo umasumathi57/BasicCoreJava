0f1ccd9aa60a71de56ee1830ace3af67ffff769a
umasumathi57 <umasumathi57@gmail.com>
2021-12-18 07:15:36 +0530

    Parsing prgm

---
+++ SriumaJava/src/main/java/excerice/console/SriumaJava/Parsing.java
@@ -0,0 +1,29 @@
+package excerice.console.SriumaJava;
+
+
+// Parsing - String to other types ( with help of Wrapper class) ( Format = Wrapper class.parseType(input/data);
+public class Parsing 
+{
+	public static void main( String[]moni)
+	{
+		
+		/*
+		 * String temp1 = "buma";
+		System.out.println(temp1);
+		temp1="4";
+		char result=temp1.charAt(0);
+		System.out.println(result);
+		 *
+		 */
+	String major="Biology";
+	System.out.println("The major group name is"+" "+ major);
+	major="6";
+	//major="8";
+	int total=Integer.parseInt(major);
+	System.out.println("The total no of sub is"+" "+(total+3));
+	
+	}
+}
+
+		
+		
